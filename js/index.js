54ed1a1a68430b9f17172a7ce4843d366ddcfef2
Rashen <rashen598@gmail.com>
2023-09-26 16:10:03 +0530

    completed the current wether card

--- js/index.js
+++ js/index.js
@@ -10,6 +10,10 @@ const searchBtn = $("#search-btn");
 const weatherIcon = $("#weather-icon");
 const searchedCity = $("#searched-city");
 const weatherDesc = $(".weather-desc");
+const dateAndTime = $(".date-and-time");
+const currentTemp = $("#current-temp");
+const currentWs = $("#current-ws");
+const currentHumidity = $("#current_humidity");
 
 function clicked(){
     $.ajax({
@@ -20,6 +24,10 @@ function clicked(){
            searchedCity.html(resp.location.name);
            weatherIcon.attr("src",resp.current.condition.icon);
            weatherDesc.html(resp.current.condition.text);   
+           dateAndTime.html(resp.current.last_updated);
+           currentTemp.html(resp.current.temp_c + " °C");
+           currentWs.html(resp.current.wind_kph + " kmp/h");
+           currentHumidity.html(resp.current.humidity + " %");
         }
      });
 }
